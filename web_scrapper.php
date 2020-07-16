77c862d3345c66fd6668cc175a965d8bb501e593
Abhishek Dhyani <abhishek.dhyani@dubswork.com>
2020-07-16 17:34:33 +0530

    first commit

---
+++ web_scrapper.php
@@ -0,0 +1,82 @@
+<?php
+include("simple_html_dom.php");
+$completeHtml = file_get_html("https://livescore.sportzwiki.com/");
+$article = $completeHtml->find('div[id=content]');
+$divTeams = $article[0]->find("div.column-teams");
+$divMeta = $article[0]->find("div.column-meta");
+
+// foreach ($divMeta as $meta) {
+
+  // code...
+  $jsonArray = array();
+  for($i=0;$i<count($divMeta);$i++){
+
+  $jsonObject = array();
+  $jsonObject["status"] = $divMeta[$i]->find('span.status',0)->plaintext;
+  $jsonObject["day"] = $divMeta[$i]->find('span.day', 0)->plaintext;
+  $jsonObject["title"] = $divMeta[$i]->find('span.title', 0)->plaintext;
+  $jsonObject["subtitle"] = $divMeta[$i]->find('span.subtitle', 0)->plaintext;
+
+  $jsonObject["match_title"] = $divMeta[$i]->find('div.title', 0)->plaintext;
+  $jsonObject["match_subtitle"] = $divMeta[$i]->find('div.subtitle', 0)->plaintext;
+  $jsonObject["match_venue"] = $divMeta[$i]->find('div.venue', 0)->plaintext;
+  $jsonObject["match_time"] = $divMeta[$i]->find('span.time', 0)->plaintext;
+
+  $teamA = $divTeams[$i]->find('div.teama', 0);
+  if (($teamA->find('div.teamLogo',0)->find('a, img',0))) {
+    // code...
+    $jsonObject["first_team_logo"] =  $teamA->find('div.teamLogo',0)->find('a, img',0)->src;
+  } else {
+    $jsonObject["first_team_logo"] =  'NA';
+  }
+  $jsonObject["first_team_name"] = $teamA->find('div.teamName', 0)->plaintext;
+
+  $jsonObject["first_team_abbr"] = $teamA->find('div.teamAbbr', 0)->plaintext;
+
+  if(($teamA->find('span.inningScore', 0))) {
+    $jsonObject["first_team_inningscore"] = $teamA->find('span.inningScore', 0)->plaintext;
+}else{
+  $jsonObject["first_team_inningscore"] = 'NA';
+}
+
+if(($teamA->find('span.inningScore', 1))) {
+  $jsonObject["first_team_inningscore2"] = $teamA->find('span.inningScore', 1)->plaintext;
+}else {
+  $jsonObject["first_team_inningscore2"] = 'NA';
+
+}
+
+  $teamB = $divTeams[$i]->find('div.teamb', 0);
+
+  if (($teamB->find('div.teamLogo',0)->find('a, img',0))) {
+    // code...
+    $jsonObject["second_team_logo"] =  $teamB->find('div.teamLogo',0)->find('a, img',0)->src;
+  } else {
+    $jsonObject["second_team_logo"] =  "NA";
+  }
+    $jsonObject["second_team_name"] = $teamB->find('div.teamName', 0)->plaintext;
+  $jsonObject["second_team_abbr"] = $teamB->find('div.teamAbbr', 0)->plaintext;
+
+  if(($teamB->find('span.inningScore', 0))) {
+    $jsonObject["second_team_inningscore"] = $teamB->find('span.inningScore', 0)->plaintext;
+}else{
+  $jsonObject["second_team_inningscore"] = 'NA';
+}
+
+if(($teamB->find('span.inningScore', 1))) {
+  $jsonObject["second_team_inningscore2"] = $teamB->find('span.inningScore', 1)->plaintext;
+}else {
+  $jsonObject["second_team_inningscore2"] = 'NA';
+
+}
+
+
+  //$jsonObject["first_team_inningscore2"] = $teamA->find('span.inningScore', 1)->plaintext;
+  $jsonArray[$i] = $jsonObject;
+}
+$out = array_values($jsonArray);
+
+$x = json_encode($out);
+echo $x;
+
+?>
